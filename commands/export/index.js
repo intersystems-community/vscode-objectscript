1354610bdeab44cf1f1fb4270bab206114e3e8e8
Дмитрович Олег <doublefint@gmail.com>
2018-03-13 01:20:50 +0300

    refactoring

--- commands/export/index.js
+++ commands/export/index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 //export 'mypkg.subpkg.name.cls' as /mypkg/subpkg/name.cls
-const atelier_filename = require('./doc-to-file-as-atelier')
+const asAtelier = require('./doc-to-file-as-atelier')
 const mkdir = require('./mkdir-p-sync') // mkdir -p 'path/to/file'
 
 // see module.exports
@@ -12,28 +12,21 @@ let api = {
 }
 
 // see module.exports
-let GetDoc = ( doc, fn ) => {}
-
-// see module.exports
 let log = (...msg) => console.log('cos.export:', ...msg ) 
 // export options
 let root = '.'
 let folder = 'src'
-let category = '*'
-let generated = 0
-let filter  = ''
 let atelier = false
 let doc2file = docname => docname 
 
-
 // Export one document 
-const DocExport = ( doc ) => () => {
+const ExportDoc = ( doc, next ) => ({ error, data }) => {
 
-    if ( !root ){
-        const message = 'Open folder before export - Ctrl+K, Ctrl+O'
-        const error = { message }
-        return { error, data: null }
+    if ( error ){
+        log( `GETDOC ${ doc.name }: ${ JSON.stringify( error ) }\n` )
+        return
     }
+    const { content, status } = data.result 
 
     // atelier: 'mypkg.subpkg.myclass.cls' => 'mypkg/subpkg/myclass.cls'
     const filename = doc2file( doc.name )
@@ -41,83 +34,34 @@ const DocExport = ( doc ) => () => {
     const folders = path.dirname( fullname )
 
     if ( !fs.existsSync( folders ) ) mkdir( folders )
-    fs.writeFileSync( fullname, doc.content.join( '\n' ) )
-
-    return { error: null, data: fullname }
-
-}
-
-
-const Exported = ( doc ) => ( { error, data } ) => {
-
-    if ( error ){
-        log('')
-        log( `Load ${ doc.name } error: ${ JSON.stringify( error ) }` ) 
-        log('')
-        return
-    }
-
-    log( '' )
-    log( `${ doc.name } -> ${ data }` )
+    fs.writeFileSync( fullname, ( content || [] ).join( '\n' ) )
+    log( `${ doc.name } -> ${ fullname }. ${ status } ` )
+    next()
 
 }
 
-const Loaded = doc => ( { error, data } ) => { 
+const doclist = ( { error, data } ) => {
 
-    if ( error ){
-        return { 
-            error: { message: `load ${ doc.name } ERROR: ${ JSON.stringify( err ) }` },
-            data: null
-        }
+    if ( error ) return log( `DOCLIST: ${ JSON.stringify(error) }` )
+    const list = data.result.content
+    log(`Documents on server: ${ list.length }` )
+
+    const docfilter = d => ( d.name.substring( 0, 1 ) !== '%' ) &&
+                            ( d.cat !== 'CSP' ) &&
+                            ( d.name.substring( 0, 12 ) !== 'INFORMATION.' )
+
+    const filtered = list.filter( docfilter )
+    log( `Without CSP, %* and INFORMATION.*: ${ filtered.length}` )
+
+    const next = () => {
+        let doc = filtered.shift()
+        if ( !doc ) return
+        let cb = ExportDoc( doc, next )
+        api.getDoc( encodeURI( doc.name ), ( error, data ) => cb( { error, data } ) )
     }
+    next()
 
-    return {
-        error: null,
-        data: data.result
-    }
-
-}
-
-// https://medium.com/javascript-scene/composing-software-an-introduction-27b72500d6ea
-const pipe = (...fns) => x => fns.reduce(( y, f ) => f( y ), x );
-
-const docsExport = ( docs, cb ) => {
-
-    let doc, loaded, docExport, exported, getDoc, cb
-    while ( doc = docs.shift() ){
-
-        loaded = Loaded( doc )
-        docExport = DocExport( doc )
-        exported = Exported( doc )
-        cb = pipe( loaded, docExport, exported )
-        getDoc = GetDoc( doc, cb )
-
-        getDoc()
-
-    }
-
-}
-
-const onGetDocs = ( err, json ) => {
-
-    if ( err ) return log( 'getDocs ERROR' )
-
-    const list = json.result.content
-    log( '' )
-    log( 'list: ' + list.length )
-    const docFilter = doc => {
-        return ( doc.cat !== 'CSP' ) &&
-                 ( doc.name.substring( 0, 1 ) !== '%' ) &&
-                 ( doc.name.substring( 0, 12 ) !== 'INFORMATION.' )
-    }
-    const docs = list.filter( docFilter )
-    log( 'without % and CSP and INFORMATION: ' + docs.length )
-    log( '' )
-
-    docsExport( docs, () => {
-        log( '' )
-        log( 'Export completed.' )
-    })
+    log( 'Export completed.\n' )
 
 }
 
@@ -126,15 +70,27 @@ const onGetDocs = ( err, json ) => {
 */
 module.exports = environment => {
 
-    ( { api, log, options } = environment );
-    ( { root, folder, atelier, category, generated, filter } = options );
+    //reassign module variables
+    ( { api, log, options } = environment ); 
+    ( { root, folder, atelier } = options );
+    if ( atelier ) doc2file = doc => asAtelier( doc )
 
-    GetDoc = ( doc, fn ) => api.getDoc( encodeURI( doc.name ), ( error, data ) => fn({ error, data }) )
-
-    if ( atelier ) doc2file = docname => atelier_filename( docname )
+    // doclist options 
+    const { category, generated, filter } = options;
 
     return () => {
-        api.getDocNames( { category, generated, filter }, onGetDocs )
+
+        if ( !root ){
+            log( `COS.EXPORT: Open folder before export - Ctrl+K, Ctrl+O` ) 
+            return 
+        }
+  
+        log( 'Load list of documents...' )
+        api.getDocNames( 
+            { category, generated, filter }, //list options
+            ( error, data ) => doclist( { error, data } ) //callback wrapper
+        )
+
     }
 
 }
