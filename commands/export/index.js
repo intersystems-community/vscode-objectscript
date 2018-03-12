6564c468886cd63a58a086784ca2adfab55aab2a
Дмитрович Олег <doublefint@gmail.com>
2018-03-12 09:25:28 +0300

    refacotring, not tested

--- commands/export/index.js
+++ commands/export/index.js
@@ -10,6 +10,11 @@ let api = {
     getDocNames: ( opts, cb ) => cb( null, {} ),
     getDoc: ( docname, cb ) => cb( null, {} ) 
 }
+
+// see module.exports
+let GetDoc = ( doc, fn ) => {}
+
+// see module.exports
 let log = (...msg) => console.log('cos.export:', ...msg ) 
 // export options
 let root = '.'
@@ -18,16 +23,16 @@ let category = '*'
 let generated = 0
 let filter  = ''
 let atelier = false
-let doc2file = docname => docname
+let doc2file = docname => docname 
 
 
 // Export one document 
-const docExport = ( doc, cb ) => {
+const DocExport = ( doc ) => () => {
 
     if ( !root ){
-        log('')
-        log('Open folder before export - Ctrl+K, Ctrl+O')
-        return cb()
+        const message = 'Open folder before export - Ctrl+K, Ctrl+O'
+        const error = { message }
+        return { error, data: null }
     }
 
     // atelier: 'mypkg.subpkg.myclass.cls' => 'mypkg/subpkg/myclass.cls'
@@ -38,44 +43,63 @@ const docExport = ( doc, cb ) => {
     if ( !fs.existsSync( folders ) ) mkdir( folders )
     fs.writeFileSync( fullname, doc.content.join( '\n' ) )
 
-    log( `${ doc.name } -> ${ fullname }` )
-    cb( null, {} )
+    return { error: null, data: fullname }
 
 }
 
-const Loaded = cb => ( err, json ) => { 
 
-    if ( err ) {
+const Exported = ( doc ) => ( { error, data } ) => {
+
+    if ( error ){
         log('')
-        // doc.name ?
-        log( `ERROR!!!:  ${ JSON.stringify( err )}` ) 
+        log( `Load ${ doc.name } error: ${ JSON.stringify( error ) }` ) 
         log('')
-        return cb( err )
+        return
     }
 
-    docExport( json.result, cb )
+    log( '' )
+    log( `${ doc.name } -> ${ data }` )
 
 }
 
-const load = ( doc, cb )  => api.getDoc( encodeURI( doc.name ), Loaded( cb ) )
+const Loaded = doc => ( { error, data } ) => { 
+
+    if ( error ){
+        return { 
+            error: { message: `load ${ doc.name } ERROR: ${ JSON.stringify( err ) }` },
+            data: null
+        }
+    }
+
+    return {
+        error: null,
+        data: data.result
+    }
+
+}
+
+// https://medium.com/javascript-scene/composing-software-an-introduction-27b72500d6ea
+const pipe = (...fns) => x => fns.reduce(( y, f ) => f( y ), x );
 
 const docsExport = ( docs, cb ) => {
 
-    let doc, loadcb; 
+    let doc, loaded, docExport, exported, getDoc, cb
     while ( doc = docs.shift() ){
 
-        loadcb = ( err, data )=> { 
-            if ( err ) log( `ERROR: ${ JSON.stringify( doc ) } ${ JSON.stringify( err ) }` )
-        }
+        loaded = Loaded( doc )
+        docExport = DocExport( doc )
+        exported = Exported( doc )
+        cb = pipe( loaded, docExport, exported )
+        getDoc = GetDoc( doc, cb )
 
-        load( doc, loadcb )
+        getDoc()
 
     }
 
 }
 
 const onGetDocs = ( err, json ) => {
-    
+
     if ( err ) return log( 'getDocs ERROR' )
 
     const list = json.result.content
@@ -104,6 +128,9 @@ module.exports = environment => {
 
     ( { api, log, options } = environment );
     ( { root, folder, atelier, category, generated, filter } = options );
+
+    GetDoc = ( doc, fn ) => api.getDoc( encodeURI( doc.name ), ( error, data ) => fn({ error, data }) )
+
     if ( atelier ) doc2file = docname => atelier_filename( docname )
 
     return () => {
