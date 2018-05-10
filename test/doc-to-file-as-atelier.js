1328ee509d04bf454965713b3748ca2a6de4ea2b
Дмитрович Олег <doublefint@gmail.com>
2018-05-10 23:43:49 +0300

    upd tests

--- test/doc-to-file-as-atelier.js
+++ test/doc-to-file-as-atelier.js
@@ -1,10 +1,11 @@
 const test = require('tape')
+const { sep } = require('path')
 const doc2file = require('./../commands/export/doc-to-file-as-atelier')
 
 test( 'doc-to-file-as-ateilier', assert => {
 
   const docname = 'mypackage.subpackage.myclass.cls'
-  const expected = 'mypackage/subpackage/myclass.cls'
+  const expected = `mypackage${sep}subpackage${sep}myclass.cls`
   const actual = doc2file( docname )
 
   assert.equal( actual, expected, 'docname to filename' )
--- test/mkdir-p-sync.js
+++ test/mkdir-p-sync.js
@@ -1,11 +1,12 @@
 const test = require( 'tape' )
 const fs = require( 'fs' )
+const { sep } = require( 'path' )
 const mkdir = require( './../commands/export/mkdir-p-sync' )
 
 test( 'mkdir-p-sync', assert => {
 
     const random = new Date().getTime() + ''
-    const testdir =  `${__dirname}/commands/export/${random}`
+    const testdir =  `${__dirname}${sep}commands${sep}export${sep}${random}`
     mkdir( testdir )
 
     const expected = true
