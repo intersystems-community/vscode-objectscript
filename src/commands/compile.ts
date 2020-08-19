12894ca7116468741350c3d714d7fc9db46b6a33
gjsjohnmurray <johnm@georgejames.com>
2020-08-20 00:10:51 +0100

    fix #279 Ctrl+F7 (Import and Compile Current File) was broken

--- src/commands/compile.ts
+++ src/commands/compile.ts
@@ -204,7 +204,7 @@ export async function importAndCompile(askFLags = false, document?: vscode.TextD
   }
 
   // Do nothing if it is a local file and objectscript.conn.active is false
-  if (document.uri.scheme === "file" && !config("conn").active) {
+  if (file.uri.scheme === "file" && !config("conn").active) {
     return;
   }
 
