9734155b506b310c58a01f79d80b711bff12a724
gjsjohnmurray <johnm@georgejames.com>
2020-09-07 18:59:09 +0100

    fix linting

--- src/commands/addServerNamespaceToWorkspace.ts
+++ src/commands/addServerNamespaceToWorkspace.ts
@@ -18,28 +18,26 @@ export async function addServerNamespaceToWorkspace(): Promise<void> {
   // Get its namespace list
   let uri = vscode.Uri.parse(`isfs://${serverName}/?ns=%SYS`);
   const api = new AtelierAPI(uri);
-  const allNamespaces: string[] = await api
-    .serverInfo()
-    .then((data) => data.result.content.namespaces
-    );
+  const allNamespaces: string[] = await api.serverInfo().then((data) => data.result.content.namespaces);
   // Prepare a displayable form of its connection spec as a hint to the user
   const connSpec = await serverManagerApi.getServerSpec(serverName);
   const connDisplayString = `${connSpec.webServer.scheme}://${connSpec.webServer.host}:${connSpec.webServer.port}/${connSpec.webServer.pathPrefix}`;
   // Get user's choice of namespace
-  const namespace = await vscode.window.showQuickPick(
-    allNamespaces,
-    {
-      placeHolder: `Namespace on server '${serverName}' (${connDisplayString})`
-    }
-  );
+  const namespace = await vscode.window.showQuickPick(allNamespaces, {
+    placeHolder: `Namespace on server '${serverName}' (${connDisplayString})`,
+  });
   if (!namespace) {
     return;
   }
   // Pick between isfs and isfs-readonly
   const editable = await vscode.window.showQuickPick(
     [
-      { value: true, label: "Editable", detail: "Documents opened from this folder will be editable directly on the server." },
-      { value: false, label: "Read-only", detail: "Documents opened from this folder will be read-only." }
+      {
+        value: true,
+        label: "Editable",
+        detail: "Documents opened from this folder will be editable directly on the server.",
+      },
+      { value: false, label: "Read-only", detail: "Documents opened from this folder will be read-only." },
     ],
     { placeHolder: "Choose the mode of access" }
   );
@@ -47,24 +45,24 @@ export async function addServerNamespaceToWorkspace(): Promise<void> {
   const label = editable.value ? `${serverName}:${namespace}` : `${serverName}:${namespace} (read-only)`;
   uri = uri.with({ scheme: editable.value ? "isfs" : "isfs-readonly", query: `ns=${namespace}` });
   // Append it to the workspace
-  const added = vscode.workspace.updateWorkspaceFolders(vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders.length : 0, 0, { uri, name: label });
+  const added = vscode.workspace.updateWorkspaceFolders(
+    vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders.length : 0,
+    0,
+    { uri, name: label }
+  );
   // Switch to Explorer view so user sees the outcome
-  vscode.commands.executeCommand('workbench.view.explorer');
+  vscode.commands.executeCommand("workbench.view.explorer");
   // Handle failure
   if (!added) {
-    vscode.window.showErrorMessage(
-      "Folder not added. Maybe it already exists on the workspace.",
-      "Retry",
-      "Close"
-    )
+    vscode.window
+      .showErrorMessage("Folder not added. Maybe it already exists on the workspace.", "Retry", "Close")
       .then((value) => {
         if (value === "Retry") {
-          vscode.commands.executeCommand('vscode-objectscript.addServerNamespaceToWorkspace');
+          vscode.commands.executeCommand("vscode-objectscript.addServerNamespaceToWorkspace");
         }
       });
   }
 }
-
 
 async function getServerManagerApi(): Promise<any> {
   const targetExtension = vscode.extensions.getExtension("intersystems-community.servermanager");
