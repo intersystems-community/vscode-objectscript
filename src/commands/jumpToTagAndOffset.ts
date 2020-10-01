e2fbcff7d7802745ad076ff012d625eeb7be0a16
Pravin Barton <pravinabarton@gmail.com>
2020-10-01 17:20:08 -0400

    Fixed Jump to Tag and Offset warning message

--- src/commands/jumpToTagAndOffset.ts
+++ src/commands/jumpToTagAndOffset.ts
@@ -8,7 +8,7 @@ export async function jumpToTagAndOffset(): Promise<void> {
   }
   const nameMatch = file.name.match(/(.*)\.(int|mac)$/i);
   if (!nameMatch) {
-    vscode.window.showWarningMessage("Jump to Tag and Offset only supports .inc and .mac routines.");
+    vscode.window.showWarningMessage("Jump to Tag and Offset only supports .int and .mac routines.");
     return;
   }
   const document = vscode.window.activeTextEditor?.document;
