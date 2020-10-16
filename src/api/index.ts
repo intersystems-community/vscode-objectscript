cbefb44c82b739fa7e78691fe90f6b956dec9aed
gjsjohnmurray <johnm@georgejames.com>
2020-10-16 18:18:54 +0100

    fix #392 no longer require %SYS namespace access

--- src/api/index.ts
+++ src/api/index.ts
@@ -340,12 +340,16 @@ export class AtelierAPI {
     }
   }
 
+  /**
+   * Use the serverInfo endpoint. Connection object does not need to have specified a namespace when instantiated.
+   */
   public serverInfo(): Promise<Atelier.Response<Atelier.Content<Atelier.ServerInfo>>> {
     return this.request(0, "GET").then((info) => {
       if (info && info.result && info.result.content && info.result.content.api > 0) {
         const data = info.result.content;
         const apiVersion = data.api;
-        if (!data.namespaces.includes(this.ns.toUpperCase())) {
+        // If this is a namespace-specific connection, throw if it doesn't exist or if the user has no access to it
+        if (this.ns && !data.namespaces.includes(this.ns.toUpperCase())) {
           throw {
             code: "WrongNamespace",
             message: `This server does not have specified namespace '${this.ns}'.\n
