d579b6c9031886337171f425bece300fb105df16
gjsjohnmurray <johnm@georgejames.com>
2022-09-29 23:04:13 +0100

    Remove redundant `InputBoxManager`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -34,9 +34,6 @@ export function outputConsole(data: string[]): void {
   });
 }
 
-import { InputBoxManager } from "./inputBoxManager";
-export { InputBoxManager };
-
 // tslint:disable-next-line: interface-name
 export interface CurrentFile {
   name: string;
