9f11e262aa3987e936f538c52b695714ea98cacc
Brett Saviano <bsaviano@intersystems.com>
2022-10-27 13:29:16 -0400

    Improve CodeLenses

--- src/providers/ObjectScriptCodeLensProvider.ts
+++ src/providers/ObjectScriptCodeLensProvider.ts
@@ -7,27 +7,27 @@ export class ObjectScriptCodeLensProvider implements vscode.CodeLensProvider {
     document: vscode.TextDocument,
     token: vscode.CancellationToken
   ): vscode.ProviderResult<vscode.CodeLens[]> {
-    const result = new Array<vscode.CodeLens>();
-
-    if (document.fileName.toLowerCase().endsWith(".cls")) {
-      result.push(...this.classMethods(document));
+    if (document.languageId == "objectscript-class") {
+      return this.classMethods(document);
     }
-    if (document.fileName.toLowerCase().endsWith(".mac")) {
-      result.push(...this.routineLabels(document));
+    if (["objectscript", "objectscript-int"].includes(document.languageId)) {
+      return this.routineLabels(document);
     }
-    return result;
+    return [];
   }
 
   private classMethods(document: vscode.TextDocument): vscode.CodeLens[] {
     const file = currentFile(document);
     const result = new Array<vscode.CodeLens>();
 
-    if (!file.name.match(/\.cls$/i)) {
-      return result;
-    }
     const className = file.name.split(".").slice(0, -1).join(".");
 
     const { debugThisMethod, copyToClipboard } = config("debug");
+    if (!debugThisMethod && !copyToClipboard) {
+      // Return early if both types are turned off
+      return result;
+    }
+
     const pattern = /(?:^ClassMethod\s)([^(]+)\((.*)/i;
     let inComment = false;
     for (let i = 0; i < document.lineCount; i++) {
@@ -63,51 +63,44 @@ export class ObjectScriptCodeLensProvider implements vscode.CodeLensProvider {
     return result;
   }
 
-  private routineLabels(document: vscode.TextDocument): vscode.CodeLens[] {
+  private async routineLabels(document: vscode.TextDocument): Promise<vscode.CodeLens[]> {
     const file = currentFile(document);
     const result = new Array<vscode.CodeLens>();
 
-    if (!file.name.match(/\.mac$/i)) {
-      return result;
-    }
     const routineName = file.name.split(".").slice(0, -1).join(".");
 
     const { debugThisMethod, copyToClipboard } = config("debug");
+    if (!debugThisMethod && !copyToClipboard) {
+      // Return early if both types are turned off
+      return result;
+    }
 
     debugThisMethod && result.push(this.addDebugThisMethod(0, [`^${routineName}`, false]));
     copyToClipboard && result.push(this.addCopyToClipboard(0, [`^${routineName}`]));
 
-    let inComment = false;
-    for (let i = 1; i < document.lineCount; i++) {
-      const line = document.lineAt(i);
-      const text = this.stripLineComments(line.text);
+    const symbols: vscode.DocumentSymbol[] = await vscode.commands.executeCommand(
+      "vscode.executeDocumentSymbolProvider",
+      document.uri
+    );
+    symbols
+      .filter((symbol) => symbol.kind === vscode.SymbolKind.Method)
+      .forEach((symbol) => {
+        const line = symbol.selectionRange.start.line;
+        const labelMatch = document.lineAt(line).text.match(/^(\w[^(\n\s]+)(?:\(([^)]*)\))?/i);
+        if (labelMatch) {
+          const [, name, parens] = labelMatch;
 
-      if (text.match(/\/\*/)) {
-        inComment = true;
-      }
-
-      if (inComment) {
-        if (text.match(/\*\//)) {
-          inComment = false;
+          debugThisMethod && result.push(this.addDebugThisMethod(line, [`${name}^${routineName}`, parens !== "()"]));
+          copyToClipboard && result.push(this.addCopyToClipboard(line, [`${name}^${routineName}`]));
         }
-        continue;
-      }
-
-      const labelMatch = text.match(/^(\w[^(\n\s]+)(?:\(([^)]*)\))?/i);
-      if (labelMatch) {
-        const [, name, parens] = labelMatch;
-
-        debugThisMethod && result.push(this.addDebugThisMethod(i, [`${name}^${routineName}`, parens !== "()"]));
-        copyToClipboard && result.push(this.addCopyToClipboard(i, [`${name}^${routineName}`]));
-      }
-    }
+      });
 
     return result;
   }
 
   private addDebugThisMethod(line: number, args: any[]) {
     return new vscode.CodeLens(new vscode.Range(line, 0, line, 80), {
-      title: `Debug this Method`,
+      title: `Debug`,
       command: "vscode-objectscript.debug",
       arguments: args,
     });
