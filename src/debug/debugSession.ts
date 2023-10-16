5b7a4ab1173519bb2ff28b7a53da49e7651639b7
gjsjohnmurray <johnm@georgejames.com>
2023-10-16 10:29:42 +0100

    Proper fix for data breakpoint option not appearing on variable context menu

--- src/debug/debugSession.ts
+++ src/debug/debugSession.ts
@@ -163,9 +163,6 @@ export class ObjectScriptDebugSession extends LoggingDebugSession {
       supportsDataBreakpoints: true,
     };
 
-    // Reset this at start of each session because our logic around whether to offer data breakpoints depends on context ids of Private and Public root folders being 1 or 2
-    this._variableIdCounter = 1;
-
     try {
       const file = currentFile();
       this._workspace = file?.workspaceFolder;
@@ -487,8 +484,12 @@ export class ObjectScriptDebugSession extends LoggingDebugSession {
     response: DebugProtocol.DataBreakpointInfoResponse,
     args: DebugProtocol.DataBreakpointInfoArguments
   ): void {
-    if (args.variablesReference !== undefined && (args.variablesReference === 1 || args.variablesReference === 2)) {
-      // This is a private or public local variable
+    if (
+      args.variablesReference !== undefined &&
+      [0, 1].includes(this._contexts.get(args.variablesReference).id) &&
+      !args.name.includes("(")
+    ) {
+      // This is an unsubscripted private or public local variable
       response.body = {
         dataId: args.name,
         description: args.name,
@@ -497,7 +498,8 @@ export class ObjectScriptDebugSession extends LoggingDebugSession {
       // This is an object property or array element, or args.variablesReference is undefined
       response.body = {
         dataId: null,
-        description: "Can only set a watchpoint on a local variable",
+        // This message isn't surfaced in VS Code, which simply doesn't offer the context menu option when dataId is null
+        description: "Can only set a watchpoint on an unsubscripted local variable",
       };
     }
 
